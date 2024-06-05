f5d03db85c348cf2ddf5ae1c97980dfffb6607d4
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-06-04 21:19:50 -0500

    Completed Review model

---
+++ models/Review.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Review extends Model {}
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        restaurantId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'restaurants',
+                key: 'id',
+            },
+        },
+        rating: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },   
+    {
+        sequelize,
+        modelName: 'Review',
+        tablename: 'reviews',
+        timestamps: true,
+    },
+);
+
+module.exports = Review;
